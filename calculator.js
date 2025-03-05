9bd255c10f9a6a1e558dcf7f966642f696215994
silent <misanthrope.sd@gmail.com>
2025-03-05 23:46:40 +0530

    Init - created main file and installed jest

---
+++ calculator.js
@@ -0,0 +1,9 @@
+// calculator.js
+function add(numbers) {
+    if (!numbers) return 0;
+
+    const nums = numbers.split(/,|\n/).map(Number);
+    return nums.reduce((sum, num) => sum + num, 0);
+}
+
+module.exports = add;
